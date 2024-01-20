2f56945d8a797ca972225bf79253b80276bb1f9e
Your Name <you@example.com>
2024-01-20 17:50:12 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Is it working or should we re-try it? Thanks!
         </p>
         <a
           className="App-link"
